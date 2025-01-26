9e9a0ba57348bc3be7cd3cb9dd19ab53457cea5f
VENANCIO MAACHAIELA <vmachaiela@MacBook-Pro-de-VENANCIO.local>
2025-01-27 01:40:57 +0200

    commit 27/01

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// 06:01:24
+// 06:10:00
+
+// nested objects = Objects inside of other Objects. Allow you to represent more complex data structures, child object is enclosed by a Parent Object
+
+// Person{Adress{}, ContactInfo{}}
+// ShoppingCart{KeyBoard{}, Mouse{}, Monitor{}}
+
+const person = {
+  fullName: "Big Developer",
+  age: 20,
+  isStudent: true,
+  hobbies: ["cooking", "coding", "watch"],
+  address: {
+    street: "007 Green Street",
+    city: "Maputo",
+    country: "Moz"
+  }
+}
